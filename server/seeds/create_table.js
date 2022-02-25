df6b3a239f3bfa8768a449668db8b1df5ebcb59c
malikkurosaki <kurosakiblackangel@gmail.com>
2022-02-25 11:12:23 +0800

    20220225111211

--- server/seeds/create_table.js
+++ server/seeds/create_table.js
@@ -4,7 +4,7 @@ const prisma = new PrismaClient();
 ;(async() => {
     console.log("coba menerapkan seed")
     try {
-        let auth = await prisma.$executeRaw`delete table if exists Auth ; create table Auth ( id int primary key auto_increment, name varchar(255), email varchar(255) password varchar(255), role varchar(255), created_at timestamp default current_timestamp, updated_at timestamp default current_timestamp on update current_timestamp )`;
+        let auth = await prisma.$executeRaw`drop table if exists Auth ; create table Auth ( id int primary key auto_increment, name varchar(255), email varchar(255) password varchar(255), role varchar(255), created_at timestamp default current_timestamp, updated_at timestamp default current_timestamp on update current_timestamp )`;
         console.log(auth)
         
     } catch (error) {
